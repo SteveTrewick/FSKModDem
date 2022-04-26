00a16ce84e3f503df40cadc201a5126ae2b37cf2
Steve Trewick <steve.trewick@icloud.com>
2022-04-26 18:03:58 +0100

    sanity check/regression test

--- Tests/FSKModDemTests/FSKModDemTests.swift
+++ Tests/FSKModDemTests/FSKModDemTests.swift
@@ -2,10 +2,33 @@ import XCTest
 @testable import FSKModDem
 
 final class FSKModDemTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FSKModDem().text, "Hello, World!")
-    }
+    
+  // these are just basic sanity tests
+  
+  func testNonInverted() {
+    
+    // generate two symbols at 1200 baud, 48 KHz
+    // we should expect 40 samples of each symbol value
+    
+    let bits : [Int] = [0, 1]
+    let fsk    = FSKModDem.Modulator(baud: 1200, inverted: false)
+    let expect = [Float](repeating: -1.0, count: 40) + [Float](repeating: 1.0, count: 40)
+    let result = fsk.modulate(bits: bits)
+    
+    XCTAssert(result == expect)
+  }
+  
+  // the same, but upside down
+  
+  func testInverted() {
+    
+    let bits : [Int] = [0, 1]
+    let fsk    = FSKModDem.Modulator(baud: 1200, inverted: true)
+    let expect = [Float](repeating: 1.0, count: 40) + [Float](repeating: -1.0, count: 40)
+    let result = fsk.modulate(bits: bits)
+  
+    XCTAssert(result == expect)
+  
+  }
+  
 }
